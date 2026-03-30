3de692a76fff558561c3a1ce0223fb22c3aaac63
Alexey Yaroshenko <alexy.yaroshenko05@icloud.com>
2026-03-30 16:53:02 +0300

    correct shrink req and resp

--- functions/functions.go
+++ functions/functions.go
@@ -2,10 +2,21 @@ package functions
 
 import (
 	"bytes"
-	"io"
+	"encoding/json"
 	"net/http"
 	"os"
 )
+
+type FullResponse struct {
+	Location    string
+	ContentType string
+	Body        struct {
+		Input struct {
+			Size int    `json:"size"`
+			Type string `json:"type"`
+		} `json:"input"`
+	}
+}
 
 func image_resize(path string) (string, error) {
 	data, err := os.ReadFile(path)
@@ -14,20 +25,23 @@ func image_resize(path string) (string, error) {
 	}
 
 	req, err := http.NewRequest("POST", "https://api.tinify.com/shrink", bytes.NewBuffer(data))
+	if err != nil {
+		return "", err
+	}
 
 	req.SetBasicAuth("api", API)
 	req.Header.Set("Host", "api.tinify.com")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
-
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-
-	return string(body), nil
+	var full FullResponse
+	full.Location = resp.Header.Get("Location")
+	full.ContentType = resp.Header.Get("Content-Type")
+	json.NewDecoder(resp.Body).Decode(&full.Body)
 
 }
